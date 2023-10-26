a1659d65a3d3fd9bf93b8233a8b59d4e73c99e97
VitoriaCardozo <126196994+Bitroia@users.noreply.github.com>
2023-10-26 11:50:42 -0300

    Initial commit

---
+++ lib/Home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String _preco ="0";
+
+  void _recuperarPreco() async {
+
+    Uri url = Uri.parse("https://blockchain.info/ticker");
+    http.Response response = await http.get(url);
+
+    Map<String, dynamic> retorno = jsonDecode(response.body);
+    setState(() {
+      _preco = retorno["BRL"]["buy"].toString();
+    });
+    print("Resultado: " + retorno["BRL"] ["buy"].toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.all(32),
+          child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("Imagens/bitcoin.png"),
+                  Padding(
+                      padding: EdgeInsets.only(top: 30,bottom: 30),
+                    child: Text(
+                        "R\$ " + _preco,
+                      style: TextStyle(
+                        fontSize: 35
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: Text("Atualizar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
+                    ),
+                    onPressed:(){
+                      _recuperarPreco();
+                    }
+                  ),
+                ],
+              )
+          )
+      ),
+      );
+  }
+}
